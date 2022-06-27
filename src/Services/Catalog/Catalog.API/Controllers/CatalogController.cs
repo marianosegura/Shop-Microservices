a92b613f682ba7ccf520863ec70b0a7810d3d716
marianosegura <luismariano10del4@gmail.com>
2022-06-27 12:52:20 -0600

    Added catalog api and configured catalog api+mongo in docker compose

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,85 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]  // aka api/v1/catalog
+    public class CatalogController : Controller
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]  // happy path return type
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);  // we use Ok (and other actions) due to returning a wrapper ActionResult
+        }
+        
+        
+        [HttpGet("{id:length(24)}", Name = "GetProduct")] 
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] 
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]  // we list other error actions returned by this endpoint
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with id: {id}, not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [Route("Category/{category}", Name = "GetProductsByCategory")]  // action=GetProductsByCategory (from method name)        
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)] 
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsByCategory(string category)
+        {
+            var products = await _repository.GetProductsByCategory(category);
+            return Ok(products);
+        }
+        
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] 
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);  // matches GetProductById given name (Name="GetProduct")
+        }  
+
+        
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] 
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {  // IActionResult instead of ActionResult since we are not wrapping any returned object
+            return Ok(await _repository.UpdateProduct(product));  // wrap returned boolean
+        }
+
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)] 
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
+        } 
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        
+
+        public CatalogContext(IConfiguration config)  // inkect config to access db string vars
+        {
+            string connectionString = config.GetValue<string>("DatabaseSettings:ConnectionString");
+            string databaseName = config.GetValue<string>("DatabaseSettings:DatabaseName");
+            string collectionName = config.GetValue<string>("DatabaseSettings:CollectionName");
+            
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            Products = database.GetCollection<Product>(collectionName);  // provide entity class to map objects
+            CatalogContextSeed.SeedData(Products);  // seed data if not empty
+        }
+    }
+}

35c3e1a21bcd1f830f01ef2a3fb85da232e5d07b
marianosegura <luismariano10del4@gmail.com>
2022-06-28 19:57:32 -0600

    Added basket api (read, create/update, delete)

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }  // copied for performance
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,46 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public readonly IDistributedCache _redisCache;
+
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {  // get basket as string and return as ShoppingCart object
+            string basketJson = await _redisCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basketJson))
+            {
+                return null;
+            }
+            var basket = JsonConvert.DeserializeObject<ShoppingCart>(basketJson);
+            return basket;
+        }
+
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            string basketJson = JsonConvert.SerializeObject(basket);
+            await _redisCache.SetStringAsync(basket.UserName, basketJson);
+            return await GetBasket(basket.UserName);
+        }
+
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}

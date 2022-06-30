4544eba4a05648c5e592838861be02d8ac3579f1
marianosegura <luismariano10del4@gmail.com>
2022-06-29 18:36:02 -0600

    Added discount api

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<Coupon> ReadDiscount(string productName);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}

43fa3ecbf1f0c75fdfb8c9be24d4635aa2014647
marianosegura <luismariano10del4@gmail.com>
2022-07-02 22:50:29 -0600

    Integrated discount grpc service into basket service

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {  // wrapper class for the discount grpc service methods
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+
+        public async Task<CouponModel> ReadDiscount(string productName)
+        {
+            var discountRequest = new ReadDiscountRequest { ProductName = productName };
+            return await _discountProtoService.ReadDiscountAsync(discountRequest);  // calling visual studio generated method
+        }
+    }
+}
